417ef01ec5445e80d467581294ef60b8c2f55e0c
N-hotate <sadalsuud.lowenzahn317@gmail.com>
2024-01-23 06:05:42 +0900

    sumパッケージにmulti関数を追加

--- sum/index.js
+++ sum/index.js
@@ -11,4 +11,17 @@ function add(numbers) {
   return result;
 }
 
-module.exports = { add };
+/**
+ * 数値の配列を受け取って、その配列の積を返す関数
+ */
+function multi(numbers) {
+  let result = 1;
+  for (const number of numbers) {
+    result *= number;
+  }
+  return result;
+}
+
+module.exports = { 
+  add, multi 
+};
